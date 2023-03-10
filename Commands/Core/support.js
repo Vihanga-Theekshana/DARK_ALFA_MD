a9b5da2a5519442345b1a72dd58133f96cce826d
Vihanga-Theekshana <121595586+Vihanga-Theekshana@users.noreply.github.com>
2023-03-10 19:45:47 +0530

    Dark Alfa MD v1

--- Commands/Core/support.js
+++ Commands/Core/support.js
@@ -8,7 +8,7 @@ module.exports = {
     start: async(Miku, m,{pushName}) => {
         m.reply(`Check your DM *${pushName}* Senpai !\n\nI have sent you support group link personally.`)
         let botpic = botImage1
-        let txt = `      🧣 *Support Group* 🧣\n\n*${botName}* is an open source project, and we are always happy to help you.\n\n*Link:*https://vihangatheekshana11111.on.drv.tw/bot *Note:* Please don't spam in the group, and don't message *Admins directly* without permission. Ask for help in *Group*.\n\n*Thanks for using Atlas.*`
+        let txt = `      🧣 *Support Group* 🧣\n\n*ƊƋГƘ ѦしẜƋ ⱮƊ* is an open source project, and we are always happy to help you.\n\n*Link:*https://vihangatheekshana11111.on.drv.tw/bot *Note:* Please don't spam in the group, and don't message *Admins directly* without permission. Ask for help in *Group*.\n\n*Thanks for using Dark Alfa MD  created by MR Theekshana.*`
         await Miku.sendMessage(m.sender,{image:{url:botpic}, caption:txt},{quoted:m})
     }
 }
